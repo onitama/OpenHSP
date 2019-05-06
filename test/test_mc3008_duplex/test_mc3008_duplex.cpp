242d2aac174f00001aa7be010cee10787ac68b2e
RollMan <qwpmb554@gmail.com>
2019-05-06 12:11:21 +0900

    have written a test
    test is gonna executing on a rasbperry pi.

--- test/test_mc3008_duplex/test_mc3008_duplex.cpp
+++ test/test_mc3008_duplex/test_mc3008_duplex.cpp
@@ -7,9 +7,17 @@ extern int SPI_ReadWord( int ch );
 extern int SPI_WriteByte( int ch, int value, int length );
 extern int MCP3008_FullDuplex(int spich, int adcch);
 
+#include <iostream>
+using std::cout;
+using std::endl;
 
 int main(){
   SPI_Init();
-  MCP3008_FullDuplex(0, 0);
+  SPI_Open(0, 0);
+
+  for(int loop = 0; loop < 100; loop++){
+    cout << MCP3008_FullDuplex(0, 0) << endl;
+  }
+  SPI_Close(0);
   return 0;
 }
