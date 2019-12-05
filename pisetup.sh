246435c7c38bf6fd4215f5b1e6fa78a02c8871bf
onitama <onitama@onionsoft.net>
2019-12-05 19:16:29 +0900

    Update Manuals

--- pisetup.sh
+++ pisetup.sh
@@ -5,6 +5,7 @@ sudo apt-get update
 sudo apt-get install --yes libgtk2.0-dev
 sudo apt-get install --yes libglew-dev
 sudo apt-get install --yes libsdl1.2-dev libsdl-image1.2-dev libsdl-mixer1.2-dev libsdl-ttf2.0-dev
+sudo apt-get install --yes libegl1-mesa-dev
 make -f makefile.raspbian
 sudo cp hsed.png /usr/share/pixmaps/
 sudo cp hsed.desktop /usr/share/applications/
