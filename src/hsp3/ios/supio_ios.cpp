44b2f4cb8b6a85d5e99aef3401639c762eec5100
Kensuke Matsuzaki <knsk.mtzk@gmail.com>
2026-08-30 22:45:39 +0900

    Fix POSIX delete return values

--- src/hsp3/ios/supio_ios.cpp
+++ src/hsp3/ios/supio_ios.cpp
@@ -311,7 +311,7 @@ int changedir( const char *name )
 
 int delfile( const char *name )
 {
-	return unlink( name );
+	return unlink( name ) == 0;
 	//return remove( name );		// ディレクトリにもファイルにも対応
 }
 
--- src/hsp3/linux/supio_linux.cpp
+++ src/hsp3/linux/supio_linux.cpp
@@ -368,7 +368,7 @@ int changedir( const char *name )
 
 int delfile( const char *name )
 {
-	return unlink( name );
+	return unlink( name ) == 0;
 	//return remove( name );		// ディレクトリにもファイルにも対応
 }
 
